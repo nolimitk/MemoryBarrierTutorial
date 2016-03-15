63231a17116556cd06820d85df3f2d21243193e9
nolimitk <nolimitk@gmail.com>
2016-03-16 07:25:17 +0900

    memory barrier problem

---
+++ MemoryBarrierTutorial/MemoryBarrierProblem.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iostream>
+#include <thread>
+#include <sstream>
+
+namespace MemoryBarrierProblem
+{
+	int g1 = 0;
+	int g2 = 0;
+
+	void func1(void)
+	{
+		std::wstringstream log_stream;
+
+		while (g1 == 0);
+		g2 = 1;
+
+		log_stream << L"func1 g1 " << g1 << L" g2 " << g2 << std::endl;
+		std::wcout << log_stream.str();
+	}
+
+	void func2(void)
+	{
+		std::wstringstream log_stream;
+
+		g2 = 2;
+		g1 = 1;
+
+		log_stream << L"func2 g1 " << g1 << L" g2 " << g2 << std::endl;
+		std::wcout << log_stream.str();
+	}
+
+	void run(void)
+	{
+		std::thread t1(func1);
+		std::thread t2(func2);
+
+		t1.join();
+		t2.join();
+	}
+}
---
+++ MemoryBarrierTutorial/MemoryBarrierTutorial.cpp
@@ -0,0 +1,12 @@
+// MemoryBarrierTutorial.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "MemoryBarrierProblem.h"
+
+int main()
+{
+	MemoryBarrierProblem::run();
+	
+    return 0;
+}
+
