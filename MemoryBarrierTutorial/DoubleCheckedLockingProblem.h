bb69dbd8f82ec64e61a59e0856a2e6b4f696083c
nolimitk <nolimitk@gmail.com>
2016-03-26 12:20:52 +0900

    double checked locking

--- MemoryBarrierTutorial/DoubleCheckedLockingProblem.h
+++ MemoryBarrierTutorial/DoubleCheckedLockingProblem.h
@@ -1,38 +1,46 @@
 #pragma once
 
-#include <windows.h>
-//#include <atomic>
-//#include "NKSingleton.h"
-//#include <strsafe.h>
+#include <atomic>
 
 namespace DoubleCheckedLockingProblem
 {
 	char* p = nullptr;
-	//int g1 = 0;
-	//volatile int g1 = 0;
-	//std::atomic<int> g2 = 0;
-	//volatile int g2 = 0;
-	unsigned int g2 = 0;
+	std::atomic<int> c = 0;
+	std::atomic<int> g1 = 0;
 
 	char* getInstance(void)
 	{
 		if (p == nullptr)
 		{
-			p = new char[128];
-			InterlockedIncrement(&g2);
-			//g2++;
+			if (p == nullptr)
+			{
+				// this is for occuring multi thread problem
+				std::this_thread::yield();
+				p = new char[256];
+				c++;
+			}
+			g1++;
 		}
 		return p;
 	}
 
+	void func(void)
+	{
+		char *p = getInstance();
+
+		std::wstringstream log_stream;
+		log_stream << L"func p " << (void *)p << std::endl;
+		std::wcout << log_stream.str();
+	}
+
 	void run(void)
 	{
-		const static int NUM_THREADS = 100;
+		const static int NUM_THREADS = 1000;
 		std::thread t[NUM_THREADS];
 
 		{
 			std::wstringstream log_stream;
-			log_stream << L"main g2 " << g2 << std::endl;
+			log_stream << L"main g2 " << c << std::endl;
 			std::wcout << log_stream.str();
 		}
 
@@ -40,13 +48,12 @@ namespace DoubleCheckedLockingProblem
 		{
 			t[i] = std::thread([i](void)->void
 				{
+					std::thread::id id = std::this_thread::get_id();
+					std::this_thread::yield();
 					char *p = getInstance();
 
-					//StringCchCopy(p, 128, "abc");
-
 					std::wstringstream log_stream;
-					//log_stream << L"lambda i " << i << " p " << (void *)p << L" g2 " << g2 << std::endl;
-					log_stream << L"lambda i " << i << " p " << (void *)p << std::endl;
+					log_stream << L"lambda thread " << id << " pointer " << (void *)p << std::endl;
 					std::wcout << log_stream.str();
 				}
 			);
@@ -59,7 +66,7 @@ namespace DoubleCheckedLockingProblem
 
 		{
 			std::wstringstream log_stream;
-			log_stream << L"main g2 " << g2 << std::endl;
+			log_stream << L"main g1 " << g1 << " count " << c << std::endl;
 			std::wcout << log_stream.str();
 		}
 	}
--- MemoryBarrierTutorial/MemoryBarrierTutorial.cpp
+++ MemoryBarrierTutorial/MemoryBarrierTutorial.cpp
@@ -6,8 +6,75 @@
 #include "MemoryBarrierProblem.h"
 #include "DoubleCheckedLockingProblem.h"
 
+#include "NKThread.h"
+
+int g1 = 0;
+int count1 = 0;
+int count2 = 0;
+
+//void producer(void)
+//{
+//	g1++;
+//	count1++;
+//}
+//
+//void consumer(void)
+//{
+//	g1--;
+//	count2++;
+//}
+
+unsigned int __stdcall producer(void*)
+{
+	for (int i = 0; i < 100; ++i)
+	{
+		g1++;
+		Sleep(0);
+	}
+	count1++;
+	return 0;
+}
+
+unsigned int __stdcall consumer(void*)
+{
+	for (int i = 0; i < 100; ++i)
+	{
+		g1--;
+		Sleep(0);
+	}
+	count2++;
+	return 0;
+}
+
 int main()
 {
+	/*
+	const static int NUM_THREADS = 10;
+	//std::thread t_p[NUM_THREADS];
+	//std::thread t_c[NUM_THREADS];
+	NKCore::NKThread t_p[NUM_THREADS];
+	NKCore::NKThread t_c[NUM_THREADS];
+
+	for (int i = 0; i < NUM_THREADS; ++i)
+	{
+		//t_p[i] = std::thread(producer);
+		//t_c[i] = std::thread(consumer);
+
+		t_p[i].start(producer);
+		t_c[i].start(consumer);
+	}
+
+	for (int i = 0; i < NUM_THREADS; ++i)
+	{
+		t_p[i].join();
+		t_c[i].join();
+	}
+
+	std::wstringstream log_stream;
+	log_stream << L"main g1 " << g1 << " count " << count1 << " , " << count2 << std::endl;
+	std::wcout << log_stream.str();
+	*/
+
 	//VolatileProblem::run();
 	//VolatileProblemWithInterlocked::run();
 	//MemoryBarrierProblem::run();
